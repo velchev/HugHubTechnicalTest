ac949d4a132249242b15aada5bcea62fd70d0af8
Lyubomir Velchev <lyubomir.velchev@gmail.com>
2019-11-18 18:08:37 +0000

    adding testing project - to do black box testing before code refactoring

---
+++ ConsoleApp1.Tests/BlackBoxTests_To_Verify_The_System_Before_Refactoring.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace ConsoleApp1.Tests
+{
+    [TestFixture]
+    public class BlackBoxTests_To_Verify_The_System_Before_Refactoring
+    {
+        [Test]
+        public void VerifyCorrectResult_With_Sample_Data()
+        {
+            var request = new PriceRequest()
+            {
+                RiskData = new RiskData() //hardcoded here, but would normally be from user input above
+                {
+                    DOB = DateTime.Parse("1980-01-01"),
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Make = "Cool New Phone",
+                    Value = 500
+                }
+            };
+
+            decimal tax = 0;
+            string insurer = "";
+            string error = "";
+
+            var priceEngine = new PriceEngine();
+            var price = priceEngine.GetPrice(request, out tax, out insurer, out error);
+
+            Assert.That(price, Is.EqualTo(92.67));
+            Assert.That(insurer, Is.EqualTo("zxcvbnm"));
+            Assert.That(tax, Is.EqualTo(11.1204m));
+            Assert.That(String.Format("You price is {0}, from insurer: {1}. This includes tax of {2}", price, insurer, tax),
+                Is.EqualTo("You price is 92.67, from insurer: zxcvbnm. This includes tax of 11.1204"));
+        }
+    }
+}
---
+++ ConsoleApp1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsoleApp1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ada875dc-c359-4fdf-9636-e40b26284005")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
